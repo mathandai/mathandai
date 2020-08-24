22f6d11114df55a37ed90935e6d4952d8201ae6a
Wag0nWheels <47494569+Wag0nWheels@users.noreply.github.com>
2020-08-24 19:14:59 +0300

    add function into categorial_features issue #8

--- search_for_categorical_features/categorial_features.py
+++ search_for_categorical_features/categorial_features.py
@@ -1,1 +1,15 @@
 import numpy as np
+
+def seach_cat_features(data, k=10):
+
+    '''
+    k - порог принадлежности к категориальным признакам
+    по числу уникальных значений в признаке.
+    '''
+    cat_list = []
+    for i in range(data.shape[1]):
+        if type(data[1, i]) == np.str_:
+            cat_list.append(i)
+        if np.unique(data[:, i]).shape[0] <= k:
+            cat_list.append(i)
+    return cat_list
