f86e16272e6d48c078f600df847dc66ae03bac2e
Wag0nWheels <47494569+Wag0nWheels@users.noreply.github.com>
2020-08-24 22:47:08 +0300

    add one_hot function issue #8

--- search_for_categorical_features/categorial_features.py
+++ search_for_categorical_features/categorial_features.py
@@ -13,3 +13,16 @@ def seach_cat_features(data, k=10):
         if np.unique(data[:, i]).shape[0] <= k:
             cat_list.append(i)
     return cat_list
+
+
+def one_hot_encoder(data, num_feature):
+    # создаем массив из нуляей
+    A = np.zeros((data.shape[0], np.unique(data[:, num_feature]).shape[0]))
+    # создаем словарь {Значение кат.признака : число}
+    dict_for_dummy = {}
+    dict_for_dummy = {j: i for i, j in enumerate(np.unique(data[:, num_feature]))}
+    for i in range(data.shape[0]):
+        A[i, dict_for_dummy[data[i, num_feature]]] = 1
+
+    return A
+
