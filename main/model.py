e51ca0ed6f45b76996b02a69ebdd736bf573dd79
Sergey Revkov <mail4sergey@mail.ru>
2020-08-27 22:42:16 +0300

    pep8, merge to master issue #9

--- main/model.py
+++ main/model.py
@@ -9,17 +9,17 @@ from sklearn.model_selection import train_test_split
 class Mathai():
 	def __init__(self):
 		pass
+
 	def fitpredict(self, X, y):		
-		#X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+		# X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
 		X_train, X_test, y_train, y_test = preprocessing(X, y)
 		clf = RidgeClassifier().fit(X_train, y_train)
 		y_pred = clf.predict(X_test)
 		print(accuracy_score(y_pred, y_test))
-		
 		
 
 X = np.loadtxt('../dataset/train.csv')
 y = np.loadtxt('../dataset/train_label.csv')
 		
 model = Mathai()
-model.fitpredict(X, y)	
+model.fitpredict(X, y)
