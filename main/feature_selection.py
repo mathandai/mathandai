80c0342f47f93acbafb232761ea16572d9b8b02e
Wag0nWheels <47494569+Wag0nWheels@users.noreply.github.com>
2020-08-28 18:29:14 +0300

    set add_del work algorithm issue #13

--- main/feature_selection.py
+++ main/feature_selection.py
@@ -1,6 +1,6 @@
 import numpy as np
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LinearRegression
 import copy
 
 
