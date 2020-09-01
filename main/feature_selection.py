b4a0c1067679617dae798163fed5293a292c97e8
Wag0nWheels <47494569+Wag0nWheels@users.noreply.github.com>
2020-09-01 15:57:29 +0300

    set add_del work algorithm issue #13

--- main/feature_selection.py
+++ main/feature_selection.py
@@ -5,14 +5,17 @@ import copy
 
 
 class Add_Del:
-    def __init__(self, d_add=2, d_dell=1):
+    def __init__(self, estimator=LinearRegression(), d_add=2, d_dell=1):
+        # определяем модель по которой отбираем признаки
+        self.estimator = estimator
+
+        # определяем сколько добавлять и удалять признков в 1 итерации
         self.d_add = d_add
         self.d_dell = d_dell
 
     def calcul_Q(self, subset_features):
-        model_reg = LinearRegression()
-        model_reg.fit(self.X_train[:, subset_features], self.y_train)
-        Y_pred = model_reg.predict(self.X_test[:, subset_features])
+        self.estimator.fit(self.X_train[:, subset_features], self.y_train)
+        Y_pred = self.estimator.predict(self.X_test[:, subset_features])
         return np.square(np.subtract(self.y_test, Y_pred)).mean()
 
     def feature_add(self, subset_features):
@@ -36,11 +39,14 @@ class Add_Del:
         subset_features.remove(c[0])
         return subset_features
 
-    def fit(self, X, y):
+    def fit(self, X_train, X_test, y_train, y_test):
 
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(X, y, test_size=0.3, random_state=42)
+        self.X_train = X_train
+        self.X_test = X_test
+        self.y_train = y_train
+        self.y_test = y_test
 
-        self.dim = X.shape[1]
+        self.dim = X_train.shape[1]
         t = 0
 
         self.Q_best = 1e12
@@ -53,41 +59,46 @@ class Add_Del:
         local_best_sub_features = []
 
         while True:
-            t_0 = t
-            while len(sub_features) < self.dim:
-                t += 1
+            if self.d_add > 0:
+                t_0 = t
+                while len(sub_features) < self.dim:
+                    t += 1
 
-                # добавили новый признак
-                sub_features = self.feature_add(sub_features)
-                now_Q = self.calcul_Q(sub_features)
-                history_Q.append(now_Q)
+                    # добавили новый признак
+                    sub_features = self.feature_add(sub_features)
+                    now_Q = self.calcul_Q(sub_features)
+                    history_Q.append(now_Q)
 
-                if local_Q_best > now_Q:
-                    local_Q_best = now_Q
-                    local_best_sub_features = copy.deepcopy(sub_features)
-                    t_0_add = t
+                    if local_Q_best > now_Q:
+                        local_Q_best = now_Q
+                        local_best_sub_features = copy.deepcopy(sub_features)
+                        t_0_add = t
 
-                if t - t_0 >= self.d_add:
-                    break
+                    if t - t_0 >= self.d_add:
+                        break
 
-            t_0 = t
-            sub_features = copy.deepcopy(local_best_sub_features)
-            while len(sub_features) > 0:
-                t += 1
+            if self.d_dell > 0:
+                if local_best_sub_features:
+                    sub_features = copy.deepcopy(local_best_sub_features)
+                else:
+                    sub_features = [k for k in range(self.dim)]
 
-                # удалили признак
-                sub_features = self.feature_del(sub_features)
-                now_Q = self.calcul_Q(sub_features)
+                t_0 = t
+                while len(sub_features) > 0:
+                    t += 1
 
-                history_Q.append(now_Q)
+                    # удалили признак
+                    sub_features = self.feature_del(sub_features)
+                    now_Q = self.calcul_Q(sub_features)
+                    history_Q.append(now_Q)
 
-                if local_Q_best > now_Q:
-                    local_Q_best = now_Q
-                    local_best_sub_features = copy.deepcopy(sub_features)
-                    t_0_add = t
+                    if local_Q_best > now_Q:
+                        local_Q_best = now_Q
+                        local_best_sub_features = copy.deepcopy(sub_features)
+                        t_0_add = t
 
-                if t - t_0 >= self.d_dell:
-                    break
+                    if t - t_0 >= self.d_dell:
+                        break
 
             if set(self.best_sub_features) == set(local_best_sub_features):
                 return self.Q_best, self.best_sub_features
@@ -96,5 +107,6 @@ class Add_Del:
                 self.Q_best = local_Q_best
                 self.best_sub_features = local_best_sub_features
 
+            # Вернем значение наименьшей ошибки и список признаков на котором она достигается
             else:
                 return self.Q_best, self.best_sub_features
